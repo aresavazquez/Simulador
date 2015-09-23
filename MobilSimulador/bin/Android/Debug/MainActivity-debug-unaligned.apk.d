7ed6a7e1a29a61da90654c8ba2654193174b842c
ares <ares@MacBook-de-ares.local>
2015-09-22 20:35:15 -0500

    Simulador contenido

---
+++ MobilSimulador/bin/Android/Debug/MainActivity-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\Users\Edgar\Documents\Visual Studio 2015\Projects\MobilSimulador\MobilSimulador\platforms\android\ant-build\MainActivity-debug-unaligned.apk : \
+C:\Users\Edgar\Documents\Visual Studio 2015\Projects\MobilSimulador\MobilSimulador\platforms\android\ant-build\MainActivity.ap_ \
+C:\Users\Edgar\Documents\Visual Studio 2015\Projects\MobilSimulador\MobilSimulador\platforms\android\ant-build\classes.dex \
+C:\Users\Edgar\Documents\Visual Studio 2015\Projects\MobilSimulador\MobilSimulador\platforms\android\CordovaLib\ant-build\classes.jar \
---
+++ MobilSimulador/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,27 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/de.appplant.cordova.plugin.email-composer/www/email_composer.js",
+        "id": "de.appplant.cordova.plugin.email-composer.EmailComposer",
+        "clobbers": [
+            "cordova.plugins.email",
+            "plugin.email"
+        ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.printer/www/printer.js",
+        "id": "de.appplant.cordova.plugin.printer.Printer",
+        "clobbers": [
+            "plugin.printer",
+            "cordova.plugins.printer"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "de.appplant.cordova.plugin.email-composer": "0.8.3dev",
+    "de.appplant.cordova.plugin.printer": "0.7.1"
+}
+// BOTTOM OF METADATA
+});
