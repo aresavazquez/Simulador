454128afacd63822b41269578868748d86341771
ares <ares@macbook-de-ares.lan>
2015-09-25 16:11:35 -0500

    estilos angular

---
+++ MobilSimulador/MobilSimulador/bin/Android/Debug/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Edgar\Documents\Visual Studio 2015\Projects\MobilSimulador\MobilSimulador\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\Edgar\Documents\Visual Studio 2015\Projects\MobilSimulador\MobilSimulador\platforms\android\AndroidManifest.xml \
