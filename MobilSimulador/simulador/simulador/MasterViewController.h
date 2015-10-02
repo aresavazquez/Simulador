fe5a0aea283be1551a0617537dbdec796818636b
ares <ares@MacBook-de-ares.local>
2015-10-02 14:51:42 -0500

    app terminada

---
+++ MobilSimulador/simulador/simulador/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  simulador
+//
+//  Created by ares on 02/10/15.
+//  Copyright (c) 2015 ares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
