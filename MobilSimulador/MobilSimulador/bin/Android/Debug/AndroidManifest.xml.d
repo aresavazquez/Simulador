9a0210965d34b8bbceed71edf8e2780656ae33ac
ares <ares@MacBook-de-ares.local>
2015-11-23 21:18:14 -0600

    simulador final

--- MobilSimulador/MobilSimulador/bin/Android/Debug/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Edgar\Documents\Visual Studio 2015\Projects\MobilSimulador\MobilSimulador\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\Edgar\Documents\Visual Studio 2015\Projects\MobilSimulador\MobilSimulador\platforms\android\AndroidManifest.xml \
